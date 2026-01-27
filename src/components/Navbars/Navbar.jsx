cc2ca195f6f94840d064710f12549a36ce8e88cc
octaveweb <karansproject955@gmail.com>
2026-01-27 17:07:21 +0530

    Add - Base File

---
+++ src/components/Navbars/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>Kanban Board</h2>
+      <button className='btn'>Creater Task</button>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/Sections/Section1.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Section1 = () => {
+    return (
+        <section className='section1'>
+            <div className="board">
+                <div className="task-column">
+                    <div className="hedding">
+                        <div className="left">Task Coloumn name</div>
+                        <div className="right">Count</div>
+                    </div>
+                </div>
+                <div className="task-column">
+                    <div className="hedding">
+                        <div className="left">Task Coloumn name</div>
+                        <div className="right">Count</div>
+                    </div>
+                </div>
+                <div className="task-column">
+                    <div className="hedding">
+                        <div className="left">Task Coloumn name</div>
+                        <div className="right">Count</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section1
